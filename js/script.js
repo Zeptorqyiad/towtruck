15fccb9581073069db992732431a1d023e65c3dd
Max Unanian <Zeptorqyiad99@gmail.com>
2024-08-31 03:24:15 +0500

    Finished with weight section in project

---
+++ js/script.js
@@ -0,0 +1,12 @@
+new Swiper('.swiper', {
+   loop: true,
+   spaceBetween: 32,
+   slidesPerView: 2,
+   pagination: {
+      el: '.swiper-pagination',
+   },
+   navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+   },
+})
